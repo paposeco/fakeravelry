0d88d28256ab6532cf4e7cbae634593c5bf1d395
paposeco <13892562+paposeco@users.noreply.github.com>
2021-11-22 13:55:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import React from "react";
+import Community from "./components/Community.js";
+import Login from "./components/Login.js";
+import Messages from "./components/Messages.js";
+import Notebook from "./components/Notebook.js";
+import Profile from "./components/Profile.js";
 
-function App() {
+//for github basename on browserrouter / ghpages name
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //isto tudo so aparece se estiver loggedin
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/community">Community</Link>
+            </li>
+            <li>
+              <Link to="/messages">Messages</Link>
+            </li>
+            <li>
+              <Link to="/notebook">Notebook</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <Switch>
+        <Route exact path="/">
+          <Profile />
+        </Route>
+        <Route exact path="/community">
+          <Community />
+        </Route>
+        <Route exact path="/messages">
+          <Messages />
+        </Route>
+        <Route exact path="/notebook">
+          <Notebook />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
