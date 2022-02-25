5e520296fc11dd0dd3d0e0f4a527088c0c015486
paposeco <13892562+paposeco@users.noreply.github.com>
2022-02-25 16:41:30 +0100

    new project form is done

--- src/components/NewProject.tsx
+++ src/components/NewProject.tsx
@@ -6,6 +6,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { nanoid } from "@reduxjs/toolkit";
 import { projectAdded } from "./projects/projectsSlice";
 import { addProjectToNotebook, checkUniqueProjectName } from "../Firebase";
+import Breadcrumbs from "./projects/Breadcrumbs";
+import type { ProfileInformation } from "./common/types";
+import { getUserProfileInformation } from "../Firebase";
 
 const NewProject = function() {
     const navigate = useNavigate();
@@ -15,6 +18,8 @@ const NewProject = function() {
     const [projectName, setProjectName] = useState<string>("");
     const [patternName, setPatternName] = useState<string>("");
     const user = useSelector((state: RootState) => state.userinfo.username);
+    const userid = useSelector((state: RootState) => state.userinfo.userID);
+    const projectData = useSelector((state: RootState) => state.projects);
     const [username, setUsername] = useState<string>("");
     const setFunctions = new Map([
         ["craft-select", setCraftType],
@@ -83,68 +88,156 @@ const NewProject = function() {
         return path;
     };
 
+    const [profilebreadcrumbimage, setprofilebreadcrumbimage] =
+        useState<string>("");
+    const [projectordinalnumber, setprojectordinalnumber] = useState<string>("");
+    const fetchUserProfileInformation = async () => {
+        if (userid !== "") {
+            const profileinfo: ProfileInformation | false | undefined =
+                await getUserProfileInformation(userid);
+            if (profileinfo !== undefined && profileinfo !== false) {
+                setprofilebreadcrumbimage(profileinfo.imageurl);
+            }
+        }
+    };
+
+    const transformtoordinalnumber = function(originalnumber: number) {
+        let ordinalnumber = "";
+        if (Math.floor(originalnumber / 10) === 0) {
+            switch (originalnumber) {
+                case 1:
+                    ordinalnumber = "1st";
+                    break;
+                case 2:
+                    ordinalnumber = "2nd";
+                    break;
+                case 3:
+                    ordinalnumber = "3rd";
+                    break;
+                default:
+                    ordinalnumber = originalnumber + "th";
+            }
+        } else if (
+            Math.floor(originalnumber / 10) > 0 &&
+            Math.floor(originalnumber / 10) <= 9
+        ) {
+            switch (originalnumber % 10) {
+                case 1:
+                    ordinalnumber = (originalnumber - (originalnumber % 10)) / 10 + "1st";
+                    break;
+                case 2:
+                    ordinalnumber = (originalnumber - (originalnumber % 10)) / 10 + "2nd";
+                    break;
+                case 3:
+                    ordinalnumber = (originalnumber - (originalnumber % 10)) / 10 + "3rd";
+                    break;
+                default:
+                    ordinalnumber = originalnumber + "th";
+            }
+        } else {
+            ordinalnumber = originalnumber + "th";
+        }
+        return ordinalnumber;
+    };
+    useEffect(() => {
+        fetchUserProfileInformation();
+    }, [userid]);
+
     useEffect(() => {
         setUsername(user);
     }, [user]);
 
-    return (
-        <div>
-            <form onSubmit={handlerOfSubmit}>
-                <label htmlFor="craft-select">Which craft?</label>
-                <select
-                    name="crafts"
-                    id="craft-select"
-                    value={craftType}
-                    onChange={handlerOfChange}
-                >
-                    <option value="knitting">Knitting</option>
-                    <option value="crochet">Crochet</option>
-                    <option value="loomknitting">Loom Knitting</option>
-                    <option value="machineknitting">Machine Knitting</option>
-                    <option value="weaving">Weaving</option>
-                    <option value="spinning">Spinning</option>
-                </select>
-                <label htmlFor="projectname">Name your project </label>
-                <input
-                    type="text"
-                    id="projectname"
-                    name="projectname"
-                    onChange={handlerOfChange}
-                />
+    useEffect(() => {
+        const ordinalnumber = transformtoordinalnumber(projectData.length + 1);
+        setprojectordinalnumber(ordinalnumber);
+    }, [projectData]);
 
-                <input
-                    type="radio"
-                    id="usedapattern"
-                    name="patternused"
-                    value="usedapattern"
-                    defaultChecked
-                />
-                <label htmlFor="usedapattern">I used a pattern</label>
-                <label htmlFor="patternname">Enter the pattern name</label>
-                <input
-                    type="text"
-                    id="patternname"
-                    name="patternname"
-                    onChange={handlerOfChange}
-                />
-                <input
-                    type="radio"
-                    id="didntuseapattern"
-                    name="patternused"
-                    value="didntuseapattern"
-                />
-                <label htmlFor="didntuseapattern">I didn't use a pattern</label>
-                <input
-                    type="radio"
-                    id="patternnotonrav"
-                    name="patternused"
-                    value="patternnotonrav"
-                />
-                <label htmlFor="patternnotonrav">
-                    The pattern that I used is not in Ravelry
-                </label>
-                <button type="submit">Continue</button>
-            </form>
+    return (
+        <div id="newprojectcontent">
+            <Breadcrumbs
+                username={username}
+                projectname={"new project"}
+                profileimage={profilebreadcrumbimage}
+            />
+
+            <div id="newproject">
+                <div id="newprojectprofile">
+                    <img src={profilebreadcrumbimage} alt="profileimg" />
+
+                    <p>You are adding your {projectordinalnumber} project</p>
+                </div>
+                <form onSubmit={handlerOfSubmit} id="newprojectform">
+                    <label htmlFor="craft-select" className="labelemphasis">
+                        Which craft?
+                    </label>
+                    <select
+                        name="crafts"
+                        id="craft-select"
+                        value={craftType}
+                        onChange={handlerOfChange}
+                    >
+                        <option value="knitting">Knitting</option>
+                        <option value="crochet">Crochet</option>
+                        <option value="loomknitting">Loom Knitting</option>
+                        <option value="machineknitting">Machine Knitting</option>
+                        <option value="weaving">Weaving</option>
+                        <option value="spinning">Spinning</option>
+                    </select>
+                    <label htmlFor="projectname" className="labelemphasis">
+                        Name your project{" "}
+                    </label>
+                    <input
+                        type="text"
+                        id="projectname"
+                        name="projectname"
+                        onChange={handlerOfChange}
+                    />
+                    <div className="newprojectlabel">
+                        <input
+                            type="radio"
+                            id="usedapattern"
+                            name="patternused"
+                            value="usedapattern"
+                            defaultChecked
+                        />
+                        <label htmlFor="usedapattern">I used a pattern</label>
+                    </div>
+                    <label htmlFor="patternname" className="labelemphasis">
+                        Enter the pattern name
+                    </label>
+                    <input
+                        type="text"
+                        id="patternname"
+                        name="patternname"
+                        onChange={handlerOfChange}
+                    />
+                    <div className="newprojectlabel">
+                        <input
+                            type="radio"
+                            id="didntuseapattern"
+                            name="patternused"
+                            value="didntuseapattern"
+                        />
+                        <label htmlFor="didntuseapattern" className="newprojectlabel">
+                            I didn't use a pattern
+                        </label>
+                    </div>
+                    <div className="newprojectlabel">
+                        <input
+                            type="radio"
+                            id="patternnotonrav"
+                            name="patternused"
+                            value="patternnotonrav"
+                        />
+                        <label htmlFor="patternnotonrav" className="newprojectlabel">
+                            The pattern that I used is not in Ravelry
+                        </label>
+                    </div>
+                    <button type="submit" className="genericbutton">
+                        Continue
+                    </button>
+                </form>
+            </div>
         </div>
     );
 };
